9ba1db86cec676b53cba5c0087ae0e068da717ef
poloniumoxide <ericyang256@gmail.com>
2024-10-09 16:59:23 -0600

    Create poloniumtemplate.cpp

---
+++ poloniumtemplate.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int tcs; cin >> tcs;
+	for (int z = 0; z < tcs; z++) {
+	}
+}
